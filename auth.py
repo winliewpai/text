91e799f572c77ac2698f1513e071a1f8f17c56ea
winliewpai <34805293+winliewpai@users.noreply.github.com>
2018-11-08 17:23:46 -0500

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,4 @@
+consumer_key        = 'JjXXe4nVsKjT5MXkOLqYrizDY'
+consumer_secret     = 'llEsyHc9p7UCLC6njD6MuWm8GD8WajAUVaMHRxkp9uBp3Rd95D'
+access_token        = '1055931071525801984-W4VqwPcYPsuqL9AOXwN4CeSRBYvfui'
+access_token_secret = 'NAZ06iPR9yHuejqcXnWW4wPGF1nW6e8gW5zQfxHuo92hv'
